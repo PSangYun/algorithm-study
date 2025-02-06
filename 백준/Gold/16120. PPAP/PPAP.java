6426691f16dfdbf1fca946c57a128935fe465501
PSYUN <133249953+PSangYun@users.noreply.github.com>
2025-02-07 01:21:14 +0900

    Update PPAP.java

--- 백준/Gold/16120. PPAP/PPAP.java
+++ 백준/Gold/16120. PPAP/PPAP.java
@@ -15,6 +15,7 @@ public class Main {
       else {
         if (i + 1 < str.length() && index >= 2 && str.charAt(i) == 'A'
             && str.charAt(i + 1) == 'P') { // 앞에 PP가 있고 현재 A고 다음에 P이면 PPAP가 P가 됨
+          // 처음에 str.length() > i + 1 로 했는데 런타임 오류 떴는데 이유가 반대로 쓰고 str.charAt(i + 1) == 'P' 전에 먼저 배열의 크기가 넘어가지 않는지 판별해야 함.
           index--;
           i++;
         } else { // A일 때 앞에 PP가 아니면 틀림
